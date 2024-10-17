51a5cad16adfc6210b7ab285feab762a80ca9d69
rifrifqyqy <rifqyh22@gmail.com>
2024-10-17 09:57:42 +0700

    add update data course controller function

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -14,12 +14,28 @@ class CoursesController < ApplicationController
     end
   end
 
+  def update
+    @course = Course.find(params[:id])
+    Rails.logger.debug "Course Params: #{course_params.inspect}"
+
+    if @course.update(course_params)
+      redirect_to @course, notice: 'Course was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def edit
+    @course = Course.includes(:materials).find(params[:id])
+  end
+  
   def show
     @course = Course.find(params[:id])
   end
+
   private
   def course_params
     # Izinkan nested attributes untuk materials
-    params.require(:course).permit(:title, :thumbnail, :description, materials_attributes: [:title, :description, :content, :_destroy])
+    params.require(:course).permit(:title, :thumbnail, :description, materials_attributes: [:id, :title, :description, :content, :_destroy])
   end
 end
